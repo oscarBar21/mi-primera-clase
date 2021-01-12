b8b6da4e10889da965943e84451fb11aa15f3830
oscar <oscarbarragan31@gmail.com>
2021-01-12 09:32:13 +0000

    Commit inicial corregido

--- Ventilador.java
+++ Ventilador.java
@@ -2,35 +2,33 @@ public class Ventilador{
     private String marca;
     private int potenciaVentilador;
     private boolean encendido;
-    private String estado;
-    
+
     public Ventilador(String marcaVentilador, int potenciaDelVentilador){
         marca = marcaVentilador;
         potenciaVentilador = potenciaDelVentilador;
         encendido = true;
-        estado = "Marca " +  marca  + " Potencia " +  potenciaVentilador  + " Encendido " + encendido;
     }
-    
+
     public String getMarca(){
         return marca;
     }
-    
+
     public int getPotenciaDelVentilador(){
         return potenciaVentilador;
     }
-    
+
     public boolean getEncendido(){
         return encendido;
     }
-    
+
     public void setMarca(String marca){
         this.marca = marca;
     }
-    
+
     public void incrementarPotencia(int nuevaPotencia){
         potenciaVentilador = potenciaVentilador + nuevaPotencia;
     }
-    
+
     public void encendido(){
         if(encendido == true){
             encendido = false;
@@ -39,12 +37,14 @@ public class Ventilador{
             encendido = true;
         }
     }
-    
+
     public void imprimirDetallesVentilador() {
         System.out.println("Marca " +  marca  + " | Potencia " +  potenciaVentilador  + " | Encendido " + encendido);
     }
     
     public String getEstado(){
+        String estado;
+        estado = ("Marca " +  marca  + " | Potencia " +  potenciaVentilador  + " | Encendido " + encendido);
         return estado;
     }
 }
